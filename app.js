7f05f8a565b2083ed25b88f4f9b50774a787e9d9
Tushar Nayan <tusharnayan2610@gmail.com>
2021-05-26 15:27:03 +0530

    Do it now

---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const { reduce } = require("lodash");
+var _ = require('lodash');
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutpageContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactpageContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const postSchema = new mongoose.Schema({ 
+  title: String,
+  content: String 
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+app.get("/",function(req,res){
+  Post.find({},function(err,posts){
+    res.render("home",{homeContent: homeStartingContent, posts: posts});
+  });
+  // console.log(posts);
+});
+
+app.get("/about",function(req,res){
+  res.render("about",{aboutContent: aboutpageContent});
+});
+
+app.get("/contact",function(req,res){
+  res.render("contact",{contactContent: contactpageContent});
+});
+
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+  
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  })
+  // Getposts.push(post);
+  // var myJSON = JSON.stringify(post);
+  // console.log(myJSON);
+});
+
+app.get("/post/:postId", function(req,res){
+  // console.log(req.params.postName);
+  // const reqestedTitle = _.lowerCase(req.params.postName);
+
+    // Getposts.forEach(function(post){
+
+    //   const responseTitle = _.lowerCase(post.title);
+      
+    //   if(reqestedTitle === responseTitle ){
+    //     res.render("post",{postTitle: post.title, postContent: post.content});
+    //   }else{
+    //     console.log("Not Matched");
+    //   }
+    // })
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+});
+
+
+app.listen(5000 || process.env.PORT , function() {
+  console.log("Server running on port 5000");
+});
